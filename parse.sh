cc164d51137a1025be271d0795c15642a8df2691
Lianqi Wang <lianqiw@gmail.com>
2023-09-13 17:00:30 -0700

    Updated for PDR2.

--- parse.sh
+++ parse.sh
@@ -1,3 +1,4 @@
-grep -H ^'    name' *ass*/command-model.conf |awk -F'[-=]' '{print "  {\n    subsystem = LGSF\n    component = "$1"\n    name ="$4 "\n  }" }' > ass_receive.conf
-grep -H ^'      name' *ass*/publish-model.conf |awk -F'[-=]' '{print "  {\n    subsystem  = LGSF\n    component  = "$1"\n    name       ="$4"\n  }" }' > ass_subscribe.conf
-grep -H ^'    name' *hcd/command-model.conf |awk -F'[-=]' '{print "  {\n    subsystem = LGSF\n    component = "$1"\n    name ="$4 "\n  }" }' > hcd_receive.conf
+echo 'Use hocon.ipynb instead. It is more accurate.'
+#grep -H ^'    name' *ass*/command-model.conf |awk -F'[-=]' '{print "  {\n    subsystem = LGSF\n    component = "$1"\n    name ="$4 "\n  }" }' > ass_receive.conf
+#grep -H ^'      name' *ass*/publish-model.conf |awk -F'[-=]' '{print "  {\n    subsystem  = LGSF\n    component  = "$1"\n    name       ="$4"\n  }" }' > ass_subscribe.conf
+#grep -H ^'    name' *hcd/command-model.conf |awk -F'[-=]' '{print "  {\n    subsystem = LGSF\n    component = "$1"\n    name ="$4 "\n  }" }' > hcd_receive.conf
